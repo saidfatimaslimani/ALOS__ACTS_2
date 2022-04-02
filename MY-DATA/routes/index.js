e7aa2dcb0cf40c6803f63569b842f056734a62bf
saidfatimaslimani <saidfatimaslimani06@gmail.com>
2022-04-02 13:29:32 +0100

    add

--- MY-DATA/routes/index.js
+++ MY-DATA/routes/index.js
@@ -11,7 +11,7 @@ db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
 
-const subscribersRouter = require('./routes/subscribers')
-app.use('/subscribers', subscribersRouter)
+const usersRouter = require('./routes/users')
+app.use('/users', usersRouter)
 
 app.listen(3000, () => console.log('Server Started'))
